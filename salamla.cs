5101f6625ab16ea7f82c5fa8a22c85595e5560aa
Azii ITClub <soft.dev.fun@gmail.com>
2017-02-19 15:24:01 +0400

    İstifadəçini salamlamaq proqramı
    Abbasov Ceyhun tərəfindən yazılmışdır

---
+++ salamla.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Adinizi daxil edin: ");
+
+        string istifadecinin_adi = Console.ReadLine();
+
+        Console.WriteLine("Salam, " + istifadecinin_adi);
+    }
+}
