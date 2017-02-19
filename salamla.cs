956535c6c60746cc165d32373b7b0f54906c6ee5
Azii ITClub <soft.dev.fun@gmail.com>
2017-02-19 15:41:58 +0400

    Update salamla.cs

--- salamla.cs
+++ salamla.cs
@@ -1,13 +1,19 @@
+/*
+    İstifadəçini salamlayan proqram.
+*/
+
 using System;
 
 class Program
 {
     static void Main()
     {
-        Console.Write("Adinizi daxil edin: ");
+        Console.Write("Adinizi daxil edin: "); //konsola "Adinizi daxil edin:" sətrini yaz.
 
-        string istifadecinin_adi = Console.ReadLine();
 
-        Console.WriteLine("Salam, " + istifadecinin_adi);
+        string istifadecinin_adi = Console.ReadLine(); //konsoldan istifadəçinin adını oxu və @istifadecinin_adi dəyişəninə mənimsət.
+
+
+        Console.WriteLine("Salam, " + istifadecinin_adi); //istifadəçinin adını konsola yaz.
     }
 }
