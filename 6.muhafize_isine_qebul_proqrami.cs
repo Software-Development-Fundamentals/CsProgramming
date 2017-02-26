6a244b775d64c31b18b5a53195466efbc5d32eb4
Azii ITClub <soft.dev.fun@gmail.com>
2017-02-26 20:58:52 +0400

    Mühafizə işinə qəbul proqramı 2
    Abbasov Ceyhun tərəfindən yaradılmışdır.
    Məqsəd: || operatorunun mənimsənməsidir.

---
+++ 6.muhafize_isine_qebul_proqrami.cs
@@ -0,0 +1,30 @@
+/*
+    İş axtaranın parametrlərinə (yaş və boy) görə
+    mühafizə işinə qəbul proqramı
+*/
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        byte yas = 18; //iş axtaranın yaşı
+        float boy = 1.70f; //iş axtaranın boyu
+
+
+        /*
+            Əgər iş axtaranın
+            yaşı 18dən böyükdürsə və ya boyu 1.70dən çoxdursa,
+            mühafizə işinə qəbul olunsun.
+        */
+        if(yas > 18 || boy > 1.70)
+        {
+            Console.WriteLine("Siz mühafizə işinə qəbul olundunuz");
+        }
+        else
+        {
+            Console.WriteLine("Siz mühafizə işinə qəbul olunmadınız");
+        }
+    }
+}
