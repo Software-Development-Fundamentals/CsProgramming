26011bacb55964f1f9065989238589c2ec46d776
Azii ITClub <soft.dev.fun@gmail.com>
2017-04-05 19:51:58 +0400

    % Operatoru və Üçlü Operator (? :)

---
+++ operatorlar/remainder_and_ternary_operator.cs
@@ -0,0 +1,22 @@
+﻿/*
+	İstifadəçinin daxil etdiyi ədədin cüt və ya tək olduğunu tapan proqram.
+*/
+
+using System;
+
+class Program
+{
+	static void Main()
+	{
+		double number = 0;
+		string input = null;
+		string output = null;
+
+		Console.Write("Ededi daxil et: ");
+		input = Console.ReadLine();
+		number = Convert.ToDouble(input);
+
+		output = (number % 2 == 0) ? "eded cutdur" : "eded tekdir";
+		Console.WriteLine(output);
+	}
+}
