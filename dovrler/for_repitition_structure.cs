a1247bdb4e3a9ebf852f04c00bc66dc7802ddc81
Azii ITClub <soft.dev.fun@gmail.com>
2017-04-05 19:28:02 +0400

    For Dövr Strukturu

---
+++ dovrler/for_repitition_structure.cs
@@ -0,0 +1,27 @@
+﻿/*
+	 İstidəçinin daxil etdiyi ədədin vurma cədvəlini konsola yazan proqram.
+	 Məqsəd: for dövr strukturunun mənimsənməsi.
+*/
+
+using System;
+
+class Program
+{
+	static void Main()
+	{
+		int number = 0;
+		string input = null;
+
+		Console.Write("Eded daxil et: ");
+		input = Console.ReadLine();
+		number = Convert.ToInt32(input);
+
+
+		Console.WriteLine();
+		Console.WriteLine(number + " ededinin vurma cedveli: ");
+		for(int i = 0; i <= 10; i++)
+		{
+			Console.WriteLine(number + " * " + i + " = " + number * i);
+		}
+	}
+}
