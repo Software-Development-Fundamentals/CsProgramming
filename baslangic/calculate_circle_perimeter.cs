f9f1578b658d58a09bf2280079eff84a9ad4e02b
Azii ITClub <soft.dev.fun@gmail.com>
2017-04-05 19:38:53 +0400

    Çevrənin radiusunu hesablayan proqram

---
+++ baslangic/calculate_circle_perimeter.cs
@@ -0,0 +1,27 @@
+﻿/*
+	 İstidəçinin daxil etdiyi ədədin vurma cədvəlini konsola yazan proqram.
+	 Məqsəd: for dövr strukturunun mənimsənməsi.
+*/
+
+using System;
+
+class Program
+{
+	static void Main()
+	{
+		double radius = 0;
+		double perimeter = 0;
+		string input = null;
+		const double pi = 3.14;	//PI ədədinin qiyməti sabitdir. 
+								//C#-da qiyməti sabit dəyişənləri göstərmək üçün 
+								//dəyişənin qarşısında const yazırıq.
+
+
+		Console.Write("Cevrenin radiusunu daxil et: ");
+		input = Console.ReadLine();
+		radius = Convert.ToDouble(input);
+		perimeter = 2 * pi * radius; 
+
+		Console.WriteLine("Cevrenin radiusu: " + perimeter);
+	}
+}
