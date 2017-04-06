1cc8d53fee809e8e452a850f9ffeee57b8aa6334
Azii ITClub <soft.dev.fun@gmail.com>
2017-04-06 17:59:51 +0400

    Rekursiv Metod. Faktorialı hesablayan proqram

--- metodlar/recursion.cs
+++ metodlar/recursion.cs
@@ -15,7 +15,13 @@ class Program
 		Console.WriteLine("Netice: " + result);
 	}
 
+	// number = 5 olduqda, result = num * CalcFactorial(--num) olur. Onda, 
 
+	// result = 5 * CalcFactorial(4) = 5 * 24 = 120
+	// CalcFactorial(4) = 4 * CalcFactorial(3) = 4 * 6 = 24
+	// CalcFactorial(3) = 3 * CalcFactorial(2) = 3 * 2 = 6
+	// CalcFactorial(2) = 2 * CalcFactorial(1) = 2 * 1 = 2
+	// CalcFactorial(1) = 1 * CalcFactorial(0) = 1 * 1 = 1
 	static int CalcFactorial(int num)
 	{
 		if (num == 0) return 1;
