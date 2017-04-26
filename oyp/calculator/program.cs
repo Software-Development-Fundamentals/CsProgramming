308279219631a12cc5631a90065a08cfacf5bbf8
Azii ITClub <soft.dev.fun@gmail.com>
2017-04-26 14:54:52 +0400

    Program

---
+++ oyp/calculator/program.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        double num1 = 15;
+        double num2 = 30;
+        double result = 0;
+        char operation = '*';
+
+        Calculator calculator = new Calculator(num1, num2, operation);
+        result = calculator.Calculate();
+        ShowResult(result);
+
+        result = Calculator.Add(5, 9);
+        ShowResult(result);
+    }
+
+    private static void ShowResult(double result)
+    {
+        string output = string.Format("Result is {0}", result);
+        Console.WriteLine(output);
+    }
+}
